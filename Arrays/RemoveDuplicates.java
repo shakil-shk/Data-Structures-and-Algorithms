c4ad7fae08ef7063f2b0850eaab92af18651cce9
shakil-ahammed-cse <shakil.ahammed.pro@gmail.com>
2025-12-30 13:16:57 +0600

    Update Class Name

--- Arrays/RemoveDuplicates.java
+++ Arrays/RemoveDuplicates.java
@@ -1,4 +1,4 @@
-class Solution {
+class RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) {
             return 0;
--- Arrays/RemoveElement.java
+++ Arrays/RemoveElement.java
@@ -1,4 +1,4 @@
-class Solution {
+class RemoveElement {
     public int removeElement(int[] nums, int val) {
         int k=0;
         for(int i=0;i<nums.length;i++){
